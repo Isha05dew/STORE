b31c3dfb142eb5f9f690aea0fd2a82abf3b592cd
Isha Dewangan <dewanganisha05@gmail.com>
2024-11-09 14:18:21 +0530

    Add productList in admin frontend, and add path of it in main.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,48 +1,46 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Route, RouterProvider, createRoutesFromElements } from 'react-router'
-import { createBrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 //Auth
-import Login from "./pages/Auth/Login.jsx"
-import Register from './pages/Auth/Register.jsx'
-import Profile from './pages/User/Profile.jsx'
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+import Profile from "./pages/User/Profile.jsx";
 
 // Private Route
-import PrivateRoute from './components/PrivateRoute.jsx'
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
-import AdminRoute from './pages/Admin/AdminRoute.jsx'
-import UserList from './pages/Admin/UserList.jsx'
-import CategoryList from './pages/Admin/CategoryList.jsx'
-
-
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-
-      <Route path='' element={<PrivateRoute />}>
-        <Route path='/profile' element={<Profile />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
       </Route>
 
       {/* Admin Routes */}
-      <Route path='/admin' element={<AdminRoute />}>
-        <Route path='userlist' element={<UserList />} />
-        <Route path='categorylist' element={<CategoryList />} />
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
       </Route>
-
     </Route>
   )
-)
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
-)
+);
