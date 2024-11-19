e8ec9886d26d6e53c60fc9a94e9f3e9e4b347e79
Isha Dewangan <dewanganisha05@gmail.com>
2024-11-19 12:07:56 +0530

    Fix error in productRoutes

--- backend/routers/product.routes.js
+++ backend/routers/product.routes.js
@@ -28,7 +28,7 @@ router
 
 router
     .route('/:id/reviews')
-    .post(authenticate, authorizeAdmin, checkId, addProductReview)
+    .post(authenticate, checkId, addProductReview)
 
 router.get('/top', fetchTopProducts)
 router.get('/new', fetchNewProducts)
